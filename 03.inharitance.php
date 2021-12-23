756e4aff8d91fa5ba6265f8ba7328c652b282038
Punit Katiyar <punit.kat@gmail.com>
2021-12-23 18:06:24 +0530

    Update 03.inharitance.php

--- 03.inharitance.php
+++ 03.inharitance.php
@@ -1,4 +1,31 @@
 <?php
+class Unit{
+    function __construct()
+    {
+        echo "<h1>I am Parent</h1>";
+    }
+    function fun()
+    {
+        echo "I am chiled";
+    }
+}
+
+class Book extends Unit{
+    function __construct()
+    {
+        parent::__construct();
+
+        echo "<h2>I am Book Parent</h2>";
+
+        
+    }
+    function greet()
+    {
+        echo "I am Book Chiled";
+    }
+}
+$obj2=new book();
+$obj2->fun();
 
 
 ?>
